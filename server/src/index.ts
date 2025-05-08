85bd9f9d2b2373671f1c4f7bebd2de0090f32fbd
Bhaswanth Isani <bhaswanthisani@gmail.com>
2025-05-08 11:14:53 +0530

    feat: logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,10 @@ import { Hono } from 'hono'
 import booksRoute from './routes/books/books'
 import { ENV } from './core/env'
 import authRoute from './routes/auth'
+import { logger } from 'hono/logger'
 
 const app = new Hono({ strict: false })
+	.use(logger())
 	.all('/health', async (c) => c.text('OK'))
 	.route('/auth', authRoute)
 	.route('/books', booksRoute)
